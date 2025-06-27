8d99bfc301796efa3c1399c306c67592fcaa7fa3
Otmane <otmane047@gmail.com>
2025-06-27 14:16:47 +0200

    feat: 🎸 5: Ajout de la soustraction + test

--- simple_math.py
+++ simple_math.py
@@ -2,5 +2,7 @@
 class SimpleMath:
     @staticmethod
     def add(a: int, b: int) -> int:
-        """Returns the sum of two integers."""
         return a + b
+    
+    def subtract(a: int, b: int) -> int:
+        return a - b
--- test_simple_math.py
+++ test_simple_math.py
@@ -1,12 +1,19 @@
 
 from unittest import TestCase
+from simple_math import SimpleMath
 
 
 class TestSimpleMath(TestCase):
     def test_add(self):
-        from simple_math import SimpleMath
         assert SimpleMath.add(1, 2) == 3
         assert SimpleMath.add(-1, 1) == 0
         assert SimpleMath.add(0, 0) == 0
         assert SimpleMath.add(-5, -5) == -10
         assert SimpleMath.add(100, 200) == 300
+
+    def test_subtract(self):
+        assert SimpleMath.subtract(5, 3) == 2
+        assert SimpleMath.subtract(0, 0) == 0
+        assert SimpleMath.subtract(-1, -1) == 0
+        assert SimpleMath.subtract(10, 5) == 5
+        assert SimpleMath.subtract(100, 50) == 50
